097c38bd52ee13ccc873de0047aee96af2c86a2a
zen <dmitriy.shemet@yalantis.com>
2014-06-15 16:38:08 +0300

    [update] cache specs for nested objects lookup

--- FastEasyMappingTests/Specs/FEMCacheSpec.m
+++ FastEasyMappingTests/Specs/FEMCacheSpec.m
@@ -5,6 +5,7 @@
 
 #import <Kiwi/Kiwi.h>
 #import <MagicalRecord/CoreData+MagicalRecord.h>
+#import <CMFactory/CMFixture.h>
 
 #import "Person.h"
 #import "FEMManagedObjectMapping.h"
@@ -12,37 +13,32 @@
 #import "MappingProvider.h"
 #import "Car.h"
 #import "FEMManagedObjectDeserializer.h"
+#import "FEMRelationshipMapping.h"
 
 
 SPEC_BEGIN(FEMCacheSpec)
     __block NSManagedObjectContext *context = nil;
-    __block NSDictionary *simpleRepresentation = nil;
-    __block FEMManagedObjectMapping *simpleMapping = nil;
+
 
     beforeEach(^{
         [MagicalRecord setDefaultModelFromClass:[self class]];
         [MagicalRecord setupCoreDataStackWithInMemoryStore];
 
         context = [NSManagedObjectContext MR_rootSavingContext];
-        simpleRepresentation = @{
-            @"id": @1,
-            @"model": @"i30",
-            @"year": @"2013"
-        };
-        simpleMapping = [MappingProvider carMappingWithPrimaryKey];
     });
 
     afterEach(^{
-        simpleMapping = nil;
-        simpleRepresentation = nil;
         context = nil;
         [MagicalRecord cleanUp];
     });
 
     describe(@"init", ^{
         it(@"should store NSManagedObjectContext", ^{
-            FEMCache *cache = [[FEMCache alloc] initWithMapping:simpleMapping
-                                         externalRepresentation:simpleRepresentation
+            id representation = [CMFixture buildUsingFixture:@"Car"];
+            id mapping = [MappingProvider carMapping];
+
+            FEMCache *cache = [[FEMCache alloc] initWithMapping:mapping
+                                         externalRepresentation:representation
                                                         context:context];
 
             [[cache.context should] equal:context];
@@ -50,50 +46,68 @@ SPEC_BEGIN(FEMCacheSpec)
     });
 
     describe(@"object retrieval", ^{
+        __block NSDictionary *representation = nil;
+        __block FEMManagedObjectMapping *mapping = nil;
         __block FEMCache *cache = nil;
         beforeEach(^{
-            cache = [[FEMCache alloc] initWithMapping:simpleMapping
-                               externalRepresentation:simpleRepresentation
-                                              context:context];
+            representation = @{
+                @"id": @1,
+                @"model": @"i30",
+                @"year": @"2013"
+            };
+            mapping = [MappingProvider carMappingWithPrimaryKey];
+
+            cache = [[FEMCache alloc] initWithMapping:mapping externalRepresentation:representation context:context];
         });
         afterEach(^{
+            mapping = nil;
+            representation = nil;
             cache = nil;
         });
 
         it(@"should return nil for missing object", ^{
             [[@([Car MR_countOfEntitiesWithContext:context]) should] beZero];
-            [[[cache existingObjectForRepresentation:simpleRepresentation mapping:simpleMapping] should] beNil];
+            [[[cache existingObjectForRepresentation:representation mapping:mapping] should] beNil];
+
+            id missingRepresentation = @{
+                @"id": @1,
+                @"model": @"i30",
+                @"year": @"2013"
+            };
+
+            [[[cache existingObjectForRepresentation:missingRepresentation mapping:mapping] should] beNil];
         });
 
         it(@"should add objects", ^{
             [[@([Car MR_countOfEntitiesWithContext:context]) should] beZero];
-
-            Car *car = [FEMManagedObjectDeserializer deserializeObjectExternalRepresentation:simpleRepresentation
-                                                                                usingMapping:simpleMapping
+            [[[cache existingObjectForRepresentation:representation mapping:mapping] should] beNil];
+            
+            Car *car = [FEMManagedObjectDeserializer deserializeObjectExternalRepresentation:representation
+                                                                                usingMapping:mapping
                                                                                      context:context];
-            [[[cache existingObjectForRepresentation:simpleRepresentation mapping:simpleMapping] should] beNil];
-            [cache addExistingObject:car usingMapping:simpleMapping];
-            [[[cache existingObjectForRepresentation:simpleRepresentation mapping:simpleMapping] should] equal:car];
+
+            [cache addExistingObject:car usingMapping:mapping];
+            [[[cache existingObjectForRepresentation:representation mapping:mapping] should] equal:car];
         });
 
         it(@"should return registered object", ^{
             [[@([Car MR_countOfEntitiesWithContext:context]) should] beZero];
 
-            Car *car = [FEMManagedObjectDeserializer deserializeObjectExternalRepresentation:simpleRepresentation
-                                                                                usingMapping:simpleMapping
+            Car *car = [FEMManagedObjectDeserializer deserializeObjectExternalRepresentation:representation
+                                                                                usingMapping:mapping
                                                                                      context:context];
 
             [[@(car.objectID.isTemporaryID) should] beTrue];
             [[[context objectRegisteredForID:car.objectID] should] equal:car];
 
-            [[[cache existingObjectForRepresentation:simpleRepresentation mapping:simpleMapping] should] equal:car];
+            [[[cache existingObjectForRepresentation:representation mapping:mapping] should] equal:car];
         });
 
         it(@"should return saved object", ^{
             [[@([Car MR_countOfEntitiesWithContext:context]) should] beZero];
 
-            Car *car = [FEMManagedObjectDeserializer deserializeObjectExternalRepresentation:simpleRepresentation
-                                                                                usingMapping:simpleMapping
+            Car *car = [FEMManagedObjectDeserializer deserializeObjectExternalRepresentation:representation
+                                                                                usingMapping:mapping
                                                                                      context:context];
             [[@(car.objectID.isTemporaryID) should] beTrue];
             [context MR_saveToPersistentStoreAndWait];
@@ -104,7 +118,45 @@ SPEC_BEGIN(FEMCacheSpec)
             [[[context objectRegisteredForID:car.objectID] should] beNil];
 
             car = [Car MR_findFirstInContext:context];
-            [[[cache existingObjectForRepresentation:simpleRepresentation mapping:simpleMapping] should] equal:car];
+            [[[cache existingObjectForRepresentation:representation mapping:mapping] should] equal:car];
+        });
+    });
+
+    describe(@"nested object retrieval", ^{
+        __block FEMCache *cache = nil;
+        __block NSDictionary *representation = nil;
+        __block FEMManagedObjectMapping *mapping = nil;
+        __block FEMManagedObjectMapping *carMapping = nil;
+
+        beforeEach(^{
+            representation = [CMFixture buildUsingFixture:@"PersonWithCar"];
+            mapping = [MappingProvider personWithCarMapping];
+
+            cache = [[FEMCache alloc] initWithMapping:mapping externalRepresentation:representation context:context];
+            carMapping = (id)[mapping relationshipMappingForProperty:@"car"].objectMapping;
+        });
+
+        afterEach(^{
+            cache = nil;
+            representation = nil;
+            mapping = nil;
+            carMapping = nil;
+        });
+
+        it(@"should return nil for missing nested object", ^{
+            [FEMManagedObjectDeserializer deserializeObjectExternalRepresentation:representation
+                                                                     usingMapping:mapping
+                                                                          context:context];
+            id missingObjectRepresentation = @{@"id": @2};
+
+            [[[cache existingObjectForRepresentation:missingObjectRepresentation mapping:carMapping] should] beNil];
+        });
+
+        it(@"should return existing nested object", ^{
+            Person *person = [FEMManagedObjectDeserializer deserializeObjectExternalRepresentation:representation
+                                                                                      usingMapping:mapping
+                                                                                           context:context];
+            [[[cache existingObjectForRepresentation:representation[@"car"] mapping:carMapping] should] equal:person.car];
         });
     });
 
