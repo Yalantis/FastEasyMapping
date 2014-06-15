d0f5d5b2ae60db02d031d855e0a3f2dea9ec57ed
zen <dmitriy.shemet@yalantis.com>
2014-06-15 17:52:43 +0300

    [update] managed object deserialiser spec by assignment policy

--- FastEasyMappingTests/Specs/FEMManagedObjectDeserializerSpec.m
+++ FastEasyMappingTests/Specs/FEMManagedObjectDeserializerSpec.m
@@ -18,333 +18,355 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-#import "Kiwi.h"
-#import "CMFixture.h"
-#import "CMFactory.h"
+#import <Kiwi/Kiwi.h>
+#import <CMFactory/CMFixture.h>
+#import <MagicalRecord/CoreData+MagicalRecord.h>
+
 #import "MappingProvider.h"
 #import "Person.h"
 #import "Car.h"
-#import "Phone.h"
-#import "MagicalRecord.h"
-#import "MagicalRecord+Setup.h"
-#import "NSManagedObjectContext+MagicalRecord.h"
 #import "FEMManagedObjectDeserializer.h"
-#import "NSManagedObjectContext+MagicalSaves.h"
-#import "FEMObjectDeserializer.h"
-#import "NSManagedObject+MagicalFinders.h"
-#import "NSManagedObject+MagicalRecord.h"
-#import "NSManagedObject+MagicalDataImport.h"
+#import "FEMRelationshipMapping.h"
+#import "FEMMapping.h"
+#import "FEMManagedObjectMapping.h"
 
 SPEC_BEGIN(FEMManagedObjectDeserializerSpec)
 
 describe(@"FEMManagedObjectDeserializer", ^{
-    
+
+    __block NSManagedObjectContext *moc;
+
     beforeEach(^{
         [MagicalRecord setDefaultModelFromClass:[self class]];
         [MagicalRecord setupCoreDataStackWithInMemoryStore];
+
+        moc = [NSManagedObjectContext MR_defaultContext];
     });
-    
+
     afterEach(^{
+        moc = nil;
+
         [MagicalRecord cleanUp];
     });
-    
+
     describe(@".objectFromExternalRepresentation:usingMapping:", ^{
-        
+
         context(@"a simple object", ^{
-            
-            __block NSManagedObjectContext* moc;
+
             __block Car *car;
             __block NSDictionary *externalRepresentation;
-            
+
             beforeEach(^{
-                moc = [NSManagedObjectContext MR_defaultContext];
                 externalRepresentation = [CMFixture buildUsingFixture:@"Car"];
-	            car = [FEMManagedObjectDeserializer deserializeObjectExternalRepresentation:externalRepresentation
-	                                                                           usingMapping:[MappingProvider carMapping]
-				                                                                    context:moc];
+                car = [FEMManagedObjectDeserializer deserializeObjectExternalRepresentation:externalRepresentation
+                                                                               usingMapping:[MappingProvider carMapping]
+                                                                                    context:moc];
             });
-            
+
             specify(^{
                 [car shouldNotBeNil];
             });
-            
+
             specify(^{
                 [[car.model should] equal:[externalRepresentation objectForKey:@"model"]];
             });
-            
+
             specify(^{
                 [[car.year should] equal:[externalRepresentation objectForKey:@"year"]];
             });
-            
+
         });
-        
+
         context(@"with existing object", ^{
-            
-            __block NSManagedObjectContext* moc;
             __block Car *oldCar;
             __block Car *car;
             __block NSDictionary *externalRepresentation;
-            
+
             beforeEach(^{
-                moc = [NSManagedObjectContext MR_defaultContext];
-                
                 oldCar = [NSEntityDescription insertNewObjectForEntityForName:@"Car" inManagedObjectContext:moc];
                 oldCar.carID = @(1);
                 oldCar.year = @"1980";
                 oldCar.model = @"";
                 [moc MR_saveToPersistentStoreAndWait];
-                
+
                 externalRepresentation = @{
-                    @"id": @(1),
-                    @"model": @"i30",
-                    @"year": @"2013"
+                        @"id" : @(1),
+                        @"model" : @"i30",
+                        @"year" : @"2013"
                 };
 
-	            car = [FEMManagedObjectDeserializer deserializeObjectExternalRepresentation:externalRepresentation
-	                                                                           usingMapping:[MappingProvider carMappingWithPrimaryKey]
-				                                                                    context:moc];
+                car = [FEMManagedObjectDeserializer deserializeObjectExternalRepresentation:externalRepresentation
+                                                                               usingMapping:[MappingProvider carMappingWithPrimaryKey]
+                                                                                    context:moc];
             });
-            
+
             specify(^{
                 [car shouldNotBeNil];
             });
-            
+
             specify(^{
                 [[car should] equal:oldCar];
             });
-            
+
             specify(^{
                 [[car.carID should] equal:oldCar.carID];
             });
-            
+
             specify(^{
                 [[car.model should] equal:[externalRepresentation objectForKey:@"model"]];
             });
-            
+
             specify(^{
                 [[car.year should] equal:[externalRepresentation objectForKey:@"year"]];
             });
-            
+
             specify(^{
                 [[[Car MR_findAll] should] haveCountOf:1];
             });
         });
-        
+
         context(@"don't clear missing values", ^{
-            
-            __block NSManagedObjectContext* moc;
             __block Car *oldCar;
             __block Car *car;
             __block NSDictionary *externalRepresentation;
-            
+
             beforeEach(^{
-                moc = [NSManagedObjectContext MR_defaultContext];
-                
                 oldCar = [NSEntityDescription insertNewObjectForEntityForName:@"Car" inManagedObjectContext:moc];
                 oldCar.carID = @(1);
                 oldCar.year = @"1980";
                 oldCar.model = @"";
-                
-                externalRepresentation = @{ @"id": @(1), @"model": @"i30", };
-	            car = [FEMManagedObjectDeserializer deserializeObjectExternalRepresentation:externalRepresentation
-	                                                                           usingMapping:[MappingProvider carMappingWithPrimaryKey]
-				                                                                    context:moc];
+
+                externalRepresentation = @{@"id" : @(1), @"model" : @"i30",};
+                car = [FEMManagedObjectDeserializer deserializeObjectExternalRepresentation:externalRepresentation
+                                                                               usingMapping:[MappingProvider carMappingWithPrimaryKey]
+                                                                                    context:moc];
             });
-            
+
             specify(^{
                 [[car.carID should] equal:oldCar.carID];
             });
-            
+
             specify(^{
                 [[car.model should] equal:[externalRepresentation objectForKey:@"model"]];
             });
-            
+
             specify(^{
                 [[car.year should] equal:oldCar.year];
             });
-            
+
             specify(^{
                 [[[Car MR_findAll] should] haveCountOf:1];
             });
-            
+
         });
-        
+
         context(@"with root key", ^{
-            
-            __block NSManagedObjectContext* moc;
             __block Car *car;
             __block NSDictionary *externalRepresentation;
-            
+
             beforeEach(^{
-                moc = [NSManagedObjectContext MR_defaultContext];
                 externalRepresentation = [CMFixture buildUsingFixture:@"CarWithRoot"];
-	            car = [FEMManagedObjectDeserializer deserializeObjectExternalRepresentation:externalRepresentation
-	                                                                           usingMapping:[MappingProvider carWithRootKeyMapping]
-				                                                                    context:moc];
+                car = [FEMManagedObjectDeserializer deserializeObjectExternalRepresentation:externalRepresentation
+                                                                               usingMapping:[MappingProvider carWithRootKeyMapping]
+                                                                                    context:moc];
                 externalRepresentation = [externalRepresentation objectForKey:@"car"];
             });
-            
+
             specify(^{
                 [car shouldNotBeNil];
             });
-            
+
             specify(^{
                 [[car.model should] equal:[externalRepresentation objectForKey:@"model"]];
             });
-            
+
             specify(^{
                 [[car.year should] equal:[externalRepresentation objectForKey:@"year"]];
             });
-            
+
         });
-        
+
         context(@"with nested information", ^{
-            
-            __block NSManagedObjectContext* moc;
             __block Car *car;
             __block NSDictionary *externalRepresentation;
-            
+
             beforeEach(^{
-                moc = [NSManagedObjectContext MR_defaultContext];
                 externalRepresentation = [CMFixture buildUsingFixture:@"CarWithNestedAttributes"];
-	            car = [FEMManagedObjectDeserializer deserializeObjectExternalRepresentation:externalRepresentation
-	                                                                           usingMapping:[MappingProvider carNestedAttributesMapping]
-				                                                                    context:moc];
+                car = [FEMManagedObjectDeserializer deserializeObjectExternalRepresentation:externalRepresentation
+                                                                               usingMapping:[MappingProvider carNestedAttributesMapping]
+                                                                                    context:moc];
             });
-            
+
             specify(^{
                 [car shouldNotBeNil];
             });
-            
+
             specify(^{
                 [[car.model should] equal:[externalRepresentation objectForKey:@"model"]];
             });
-            
+
             specify(^{
                 [[car.year should] equal:[[externalRepresentation objectForKey:@"information"] objectForKey:@"year"]];
             });
-            
+
         });
-        
+
         context(@"with dateformat", ^{
-            
-            __block NSManagedObjectContext* moc;
             __block Car *car;
             __block NSDictionary *externalRepresentation;
-            
+
             beforeEach(^{
                 moc = [NSManagedObjectContext MR_defaultContext];
                 externalRepresentation = [CMFixture buildUsingFixture:@"CarWithDate"];
-	            car = [FEMManagedObjectDeserializer deserializeObjectExternalRepresentation:externalRepresentation
-	                                                                           usingMapping:[MappingProvider carWithDateMapping]
-				                                                                    context:moc];
+                car = [FEMManagedObjectDeserializer deserializeObjectExternalRepresentation:externalRepresentation
+                                                                               usingMapping:[MappingProvider carWithDateMapping]
+                                                                                    context:moc];
             });
-            
+
             specify(^{
                 [car shouldNotBeNil];
             });
-            
+
             specify(^{
                 [[car.model should] equal:[externalRepresentation objectForKey:@"model"]];
             });
-            
+
             specify(^{
                 [[car.year should] equal:[externalRepresentation objectForKey:@"year"]];
             });
-            
+
             it(@"should populate createdAt property with a NSDate", ^{
-                
+
                 NSDateFormatter *format = [[NSDateFormatter alloc] init];
                 format.timeZone = [NSTimeZone timeZoneWithName:@"Europe/London"];
                 format.locale = [[NSLocale alloc] initWithLocaleIdentifier:@"en_US_POSIX"];
                 format.dateFormat = @"yyyy-MM-dd";
                 NSDate *expectedDate = [format dateFromString:[externalRepresentation objectForKey:@"created_at"]];
                 [[car.createdAt should] equal:expectedDate];
-                
+
             });
-            
+
         });
-        
+
         context(@"with hasOne objectMapping", ^{
-            
-            __block NSManagedObjectContext* moc;
             __block Person *person;
             __block Car *expectedCar;
-            
+
             beforeEach(^{
-                moc = [NSManagedObjectContext MR_defaultContext];
-                
                 expectedCar = [Car MR_createEntity];
                 expectedCar.model = @"i30";
                 expectedCar.year = @"2013";
-                
+
                 NSDictionary *externalRepresentation = [CMFixture buildUsingFixture:@"Person"];
-	            person = [FEMManagedObjectDeserializer deserializeObjectExternalRepresentation:externalRepresentation
-	                                                                              usingMapping:[MappingProvider personMapping]
-				                                                                       context:moc];
+                person = [FEMManagedObjectDeserializer deserializeObjectExternalRepresentation:externalRepresentation
+                                                                                  usingMapping:[MappingProvider personMapping]
+                                                                                       context:moc];
             });
-            
+
             specify(^{
                 [person.car shouldNotBeNil];
             });
-            
+
             specify(^{
                 [[person.car.model should] equal:expectedCar.model];
             });
-            
+
             specify(^{
                 [[person.car.year should] equal:expectedCar.year];
             });
-            
+
         });
-        
+
         context(@"with hasMany objectMapping", ^{
-            
-            __block NSManagedObjectContext* moc;
             __block Person *person;
-            
+
             beforeEach(^{
                 moc = [NSManagedObjectContext MR_defaultContext];
                 NSDictionary *externalRepresentation = [CMFixture buildUsingFixture:@"Person"];
-	            person = [FEMManagedObjectDeserializer deserializeObjectExternalRepresentation:externalRepresentation
-	                                                                              usingMapping:[MappingProvider personMapping]
-				                                                                       context:moc];
+                person = [FEMManagedObjectDeserializer deserializeObjectExternalRepresentation:externalRepresentation
+                                                                                  usingMapping:[MappingProvider personMapping]
+                                                                                       context:moc];
             });
-            
+
             specify(^{
                 [person.phones shouldNotBeNil];
             });
-            
+
             specify(^{
                 [[person.phones should] haveCountOf:2];
             });
-            
+
         });
-        
+
     });
-    
+
     describe(@".deserializeCollectionExternalRepresentation:usingMapping:", ^{
-        
-        __block NSManagedObjectContext* moc;
         __block NSArray *carsArray;
         __block NSArray *externalRepresentation;
-        
+
         beforeEach(^{
-            moc = [NSManagedObjectContext MR_defaultContext];
             externalRepresentation = [CMFixture buildUsingFixture:@"Cars"];
-	        carsArray = [FEMManagedObjectDeserializer deserializeCollectionExternalRepresentation:externalRepresentation
-	                                                                                 usingMapping:[MappingProvider carMapping]
-				                                                                          context:moc];
+            carsArray = [FEMManagedObjectDeserializer deserializeCollectionExternalRepresentation:externalRepresentation
+                                                                                     usingMapping:[MappingProvider carMapping]
+                                                                                          context:moc];
         });
-        
+
         specify(^{
             [carsArray shouldNotBeNil];
         });
-        
+
         specify(^{
             [[carsArray should] haveCountOf:[externalRepresentation count]];
         });
-        
+
+    });
+
+    describe(@"relationship assignment policy", ^{
+        __block NSDictionary *externalRepresentation_v1 = nil;
+        __block NSDictionary *externalRepresentation_v2 = nil;
+        __block FEMManagedObjectMapping *mapping = nil;
+        __block FEMRelationshipMapping *relationshipMapping = nil;
+
+        beforeEach(^{
+            externalRepresentation_v1 = [CMFixture buildUsingFixture:@"PersonWithCar_1"];
+            externalRepresentation_v2 = [CMFixture buildUsingFixture:@"PersonWithCar_2"];
+            mapping = [MappingProvider personWithCarMapping];
+            relationshipMapping = [mapping relationshipMappingForProperty:@"car"];
+        });
+
+        afterEach(^{
+            externalRepresentation_v1 = nil;
+            externalRepresentation_v2 = nil;
+            mapping = nil;
+            relationshipMapping = nil;
+        });
+
+        context(@"assign", ^{
+            it(@"should assign new value", ^{
+                relationshipMapping.assignmentPolicy = FEMRelationshipAssignmentPolicyAssign;
+                
+                [[@([Car MR_countOfEntitiesWithContext:moc]) should] beZero];
+                Person *person_v1 = [FEMManagedObjectDeserializer deserializeObjectExternalRepresentation:externalRepresentation_v1
+                                                                                             usingMapping:mapping
+                                                                                                  context:moc];
+                [moc MR_saveToPersistentStoreAndWait];
+                
+                [[@([Car MR_countOfEntitiesWithContext:moc]) should] equal:@1];
+                
+                Car *car_v1 = person_v1.car;
+                [[car_v1 should] equal:[Car MR_findFirstInContext:moc]];
+                
+                Person *person_v2 = [FEMManagedObjectDeserializer deserializeObjectExternalRepresentation:externalRepresentation_v2
+                                                                                             usingMapping:mapping
+                                                                                                  context:moc];
+                
+                [[person_v1 should] equal:person_v2];
+                Car *car_v2 = person_v1.car;
+                
+                [[car_v1 shouldNot] equal:car_v2];
+                [[car_v1.person should] beNil];
+            });
+        });
     });
 });
 
