8240fbe195a94b9566dcfbd99f8d15571398324f
zen <zen.dev@yandex.com>
2015-06-22 18:07:44 +0300

    Fix delegate methods to reflects NSNumber, NSString & NSDictionary as a valid containers

--- FastEasyMapping/Source/Core/Deserializer/FEMDeserializer.h
+++ FastEasyMapping/Source/Core/Deserializer/FEMDeserializer.h
@@ -9,8 +9,8 @@
 @protocol FEMDeserializerDelegate <NSObject>
 
 @optional
-- (void)deserializer:(FEMDeserializer *)deserializer willMapObjectFromRepresentation:(NSDictionary *)representation mapping:(FEMMapping *)mapping;
-- (void)deserializer:(FEMDeserializer *)deserializer didMapObject:(id)object fromRepresentation:(NSDictionary *)representation mapping:(FEMMapping *)mapping;
+- (void)deserializer:(FEMDeserializer *)deserializer willMapObjectFromRepresentation:(id)representation mapping:(FEMMapping *)mapping;
+- (void)deserializer:(FEMDeserializer *)deserializer didMapObject:(id)object fromRepresentation:(id)representation mapping:(FEMMapping *)mapping;
 
 - (void)deserializer:(FEMDeserializer *)deserializer willMapCollectionFromRepresentation:(NSArray *)representation mapping:(FEMMapping *)mapping;
 - (void)deserializer:(FEMDeserializer *)deserializer didMapCollection:(NSArray *)collection fromRepresentation:(NSArray *)representation mapping:(FEMMapping *)mapping;
