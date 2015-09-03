1fa48afa3caa13d08c2c6359dae337fd3799bbd0
zen <zen.dev@yandex.com>
2015-09-03 15:29:27 +0300

    Add global realm header

---
+++ FastEasyMapping-Realm/Source/FastEasyMapping-Realm.h
@@ -0,0 +1,15 @@
+//
+//  FastEasyMapping-Realm.h
+//  FastEasyMapping
+//
+//  Created by zen on 03/09/15.
+//  Copyright (c) 2015 Yalantis. All rights reserved.
+//
+
+#ifndef _FASTEASYMAPPING_REALM_
+#define _FASTEASYMAPPING_REALM_
+
+#import "FEMRealmStore.h"
+#import "FEMDeserializer+Realm.h"
+
+#endif
