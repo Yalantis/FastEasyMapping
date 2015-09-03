addd38c508e297e9dae6a40cb222bfa37659f4a2
zen <zen.dev@yandex.com>
2015-09-03 16:08:25 +0300

    Add assume_nonnull directive to assignment context

--- FastEasyMapping/Source/Core/Assignment Policy/FEMRelationshipAssignmentContext.h
+++ FastEasyMapping/Source/Core/Assignment Policy/FEMRelationshipAssignmentContext.h
@@ -7,25 +7,29 @@
 
 @class FEMRelationship, FEMObjectStore, FEMRelationshipAssignmentContext;
 
+NS_ASSUME_NONNULL_BEGIN
+
 @protocol FEMRelationshipAssignmentContextDelegate <NSObject>
 @required
 
-- (void)assignmentContext:(nonnull FEMRelationshipAssignmentContext *)context deletedObject:(nonnull id)object;
+- (void)assignmentContext:(FEMRelationshipAssignmentContext *)context deletedObject:(id)object;
 
 @end
 
 
 @interface FEMRelationshipAssignmentContext: NSObject
 
-@property (nonatomic, unsafe_unretained, readonly, nonnull) FEMObjectStore *store;
-- (nonnull instancetype)initWithStore:(nonnull FEMObjectStore *)store;
+@property (nonatomic, unsafe_unretained, readonly) FEMObjectStore *store;
+- (instancetype)initWithStore:(FEMObjectStore *)store;
 
-@property (nonatomic, strong, readonly, nonnull) id destinationObject;
-@property (nonatomic, strong, readonly, nonnull) FEMRelationship *relationship;
+@property (nonatomic, strong, readonly) id destinationObject;
+@property (nonatomic, strong, readonly) FEMRelationship *relationship;
 
 @property (nonatomic, strong, readonly, nullable) id sourceRelationshipValue;
 @property (nonatomic, strong, readonly, nullable) id targetRelationshipValue;
 
-- (void)deleteRelationshipObject:(nonnull id)object;
+- (void)deleteRelationshipObject:(id)object;
 
 @end
+
+NS_ASSUME_NONNULL_END
