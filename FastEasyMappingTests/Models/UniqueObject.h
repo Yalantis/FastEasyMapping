b7d3a743efa7eb88ec9182db7c335b1dda021dec
zen <zen.dev@yandex.com>
2015-09-11 11:57:33 +0300

    Add unique object to FEM tests
    Former-commit-id: 2fa6c485b803f74904e2b7b9d830074df707bc2a

---
+++ FastEasyMappingTests/Models/UniqueObject.h
@@ -0,0 +1,13 @@
+//
+// Created by zen on 11/09/15.
+// Copyright (c) 2015 Yalantis. All rights reserved.
+//
+
+@import Foundation;
+
+@interface UniqueObject : NSObject
+
+@property (nonatomic) NSInteger integerPrimaryKey;
+@property (nonatomic, copy) NSString *stringPrimaryKey;
+
+@end
---
+++ FastEasyMappingTests/Models/UniqueObject.m
@@ -0,0 +1,11 @@
+//
+// Created by zen on 11/09/15.
+// Copyright (c) 2015 Yalantis. All rights reserved.
+//
+
+#import "UniqueObject.h"
+
+@implementation UniqueObject {
+
+}
+@end
