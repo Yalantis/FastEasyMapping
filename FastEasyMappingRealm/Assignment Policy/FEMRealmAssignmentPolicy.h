d994445c5441d747de1bfe7f538e140c87c730c5
Dima Zen <zen.dev@yandex.com>
2017-03-28 18:05:58 +0300

    Dropped Realm included files
    Former-commit-id: 452d7868f5ec95eb777a2f2059a1f3ac78e0faec

--- FastEasyMappingRealm/Assignment Policy/FEMRealmAssignmentPolicy.h
+++
@@ -1,9 +0,0 @@
-//
-// Created by zen on 07/09/15.
-// Copyright (c) 2015 Yalantis. All rights reserved.
-//
-
-#import <FastEasyMapping/FEMAssignmentPolicy.h>
-
-OBJC_EXTERN FEMAssignmentPolicy FEMRealmAssignmentPolicyCollectionMerge;
-OBJC_EXTERN FEMAssignmentPolicy FEMRealmAssignmentPolicyCollectionReplace;
