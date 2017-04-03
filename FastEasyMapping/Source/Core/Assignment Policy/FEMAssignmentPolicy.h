d50f55f12afd0594ecd08b930b80e18fac62008e
Dima Zen <zen.dev@yandex.com>
2017-04-03 11:29:47 +0300

    Fixed typo for assignment policies description

--- FastEasyMapping/Source/Core/Assignment Policy/FEMAssignmentPolicy.h
+++ FastEasyMapping/Source/Core/Assignment Policy/FEMAssignmentPolicy.h
@@ -11,14 +11,14 @@ typedef __nullable id (^FEMAssignmentPolicy)(FEMRelationshipAssignmentContext *c
 /**
  @brief Assign new value. Previous value remains untouched. Suitable for both to-one and to-many relationship types.
  
- @discussion Given values: A (old), B (new). Result of assingment is B, A remains untouched (not removed from the database).
+ @discussion Given values: A (old), B (new). Result of assignment is B, A remains untouched (not removed from the database).
  */
 OBJC_EXTERN FEMAssignmentPolicy FEMAssignmentPolicyAssign;
 
 /**
  @brief Merge two values. Suitable for to-one relationship type.
  
- @discussion Given values: A (old), B (new). Result of assingment is either B or A in case B is nil.
+ @discussion Given values: A (old), B (new). Result of assignment is either B or A in case B is nil.
  Previous value remains untouched (not removed from the database).
  
  IMPORTANT: Designed only for to-one relationship type.
@@ -28,7 +28,7 @@ OBJC_EXTERN FEMAssignmentPolicy FEMAssignmentPolicyObjectMerge;
 /**
  @brief Merge two collections. Suitable for to-many relationship type.
  
- @discussion Given two collections: [A, B, C, D] and [1, 2, 3, 4]. Result of assingment is a merged collection: [A, B, C, D, 1, 2, 3, 4].
+ @discussion Given two collections: [A, B, C, D] and [1, 2, 3, 4]. Result of assignment is a merged collection: [A, B, C, D, 1, 2, 3, 4].
  Values that are presented in both collections do no duplicated, therefore merge of two collections [A, B] and [B, C] will result into a [A, B, C, D].
  
  Supported collections are:
@@ -42,7 +42,7 @@ OBJC_EXTERN FEMAssignmentPolicy FEMAssignmentPolicyCollectionMerge;
 /**
  @brief Replace old value by new. Suitable for to-one relationship type.
  
- @discussion Given values: A (old), B (new). Result of assingment is B. A removed from the database.
+ @discussion Given values: A (old), B (new). Result of assignment is B. A removed from the database.
  
  IMPORTANT: Designed only for to-one relationship type.
  */
@@ -51,7 +51,7 @@ OBJC_EXTERN FEMAssignmentPolicy FEMAssignmentPolicyObjectReplace;
 /**
  @brief Replace old values by new. Suitable for to-many relationship type.
  
- @discussion Given two collections: [A, B, C, D] and [1, 2, 3, 4]. Result of assingment is a collection: [1, 2, 3, 4].
+ @discussion Given two collections: [A, B, C, D] and [1, 2, 3, 4]. Result of assignment is a collection: [1, 2, 3, 4].
  Note, that [A, B, C, D] removed from the database!
  
  In case value presented in both new and old collections it . Therefore result of replacement of [A, B, C] by [C, D] is [C, D].
