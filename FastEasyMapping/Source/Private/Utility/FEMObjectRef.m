7a5aa135b9c7b20b0fab1442ed3bce8d60eb516c
Dima Zen <zen.dev@yandex.com>
2017-05-04 14:45:12 +0300

    GH-83 Add ObjectRef wrapper to control weak ownership

---
+++ FastEasyMapping/Source/Private/Utility/FEMObjectRef.m
@@ -0,0 +1,31 @@
+// For License please refer to LICENSE file in the root of FastEasyMapping project
+
+#import "FEMObjectRef.h"
+
+@implementation FEMObjectRef
+
+- (instancetype)initWithValue:(id)value {
+    self = [super init];
+    if (self) {
+        _strongValue = value;
+    }
+    return self;
+}
+
+- (void)setUseWeakOwnership:(BOOL)useWeakOwnership {
+    _useWeakOwnership = useWeakOwnership;
+    
+    if (_useWeakOwnership) {
+        _weakValue = _strongValue;
+        _strongValue = nil;
+    } else {
+        _strongValue = _weakValue;
+        _weakValue = nil;
+    }
+}
+
+- (id)value {
+    return _strongValue ?: _weakValue;
+}
+
+@end
