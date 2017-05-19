acb2a117a379746d476c1ad88c8c5ed58b4b021f
Dima Zen <zen.dev@yandex.com>
2017-05-19 21:44:14 +0300

    GH-96 Add spec for presented primary keys collecting

--- FastEasyMappingTests/Specs/FEMRepresentationUtilitySpec.m
+++ FastEasyMappingTests/Specs/FEMRepresentationUtilitySpec.m
@@ -1,24 +1,25 @@
 // For License please refer to LICENSE file in the root of FastEasyMapping project
 
 #import <Kiwi/Kiwi.h>
-#import <MagicalRecord/MagicalRecord.h>
+
+@import FastEasyMapping;
 
 #import "Fixture.h"
-
-#import "Person.h"
-#import "FEMMapping.h"
-#import "FEMObjectCache.h"
-#import "MappingProvider.h"
-#import "Car.h"
-
-#import "FEMDeserializer.h"
-#import "FEMRelationship.h"
-#import "FEMRepresentationUtility.h"
+#import "Chat+Mapping.h"
 
 SPEC_BEGIN(FEMRepresentationUtilitySpec)
 describe(@"FEMRepresentationCollectPresentedPrimaryKeys", ^{
     context(@"indirect recursive relationship", ^{
-        
+        it(@"should combine similar mappings", ^{
+            NSDictionary *fixture = [Fixture buildUsingFixture:@"RecursiveChatLastMessage"];
+            FEMMapping *mapping = [Chat chatLastMessageMapping];
+            
+            NSDictionary<NSNumber *, NSSet<id> *> *map = FEMRepresentationCollectPresentedPrimaryKeys(fixture, mapping);
+            [[[map should] have:2] values];
+            
+            [[map[mapping.uniqueIdentifier] should] equal:[NSSet setWithObject:@1]];
+            [[map[[mapping relationshipForProperty:@"lastMessage"].mapping.uniqueIdentifier] should] equal:[NSSet setWithObject:@300]];
+        });
     });
 });
 
